11ca110f3cb7ba432f72d5349880236cf774a281
dongwankim <zkdlwu94@naver.com>
2023-09-05 16:09:26 +0900

    aa

---
+++ src/main/java/com/example/springmicroservicepractice/SpringMicroservicePracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.springmicroservicepractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMicroservicePracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMicroservicePracticeApplication.class, args);
+    }
+
+}
